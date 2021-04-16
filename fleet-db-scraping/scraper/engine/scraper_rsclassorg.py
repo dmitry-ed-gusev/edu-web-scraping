e87b7c871daeccea98acb23b6a849ae916656cd7
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2021-04-16 22:58:50 +0200

    Restored working state for rs-class reg book scraper. Single-threaded execution.

--- fleet-db-scraping/scraper/engine/scraper_rsclassorg.py
+++ fleet-db-scraping/scraper/engine/scraper_rsclassorg.py
@@ -2,10 +2,10 @@
 # coding=utf-8
 
 """
-    Scraper for RMRS Register Book :)
+    Scraper for RMRS Register Book.
 
     Created:  Gusev Dmitrii, 10.01.2021
-    Modified: Gusev Dmitrii, 06.04.2021
+    Modified: Gusev Dmitrii, 16.04.2021
 """
 
 
@@ -34,7 +34,8 @@ log = logging.getLogger('scraper_rsclassorg')
 # todo: extract hashmap and search variations generation into separated class/script???
 
 def get_hash_bucket_number(value, buckets):
-    """Generate hash bucket number for the given value and buckets count.
+    """Generate hash bucket number for the given value, generated bucket number
+    will be less than provided buckets count.
     :param value:
     :param buckets:
     :return:
@@ -44,7 +45,7 @@ def get_hash_bucket_number(value, buckets):
     if value is None or len(value.strip()) == 0:  # fail-fast if value is empty
         raise ValueError('Provided empty value!')
 
-    if buckets <= 0:  # if buckets number <= 0 - bucket number is always 0
+    if buckets <= 0:  # if buckets number <= 0 - generated bucket number is always 0
         log.debug('get_hash_bucket_number(): buckets number [{}] is <= 0, return 0!'.format(buckets))
         return 0
 
@@ -59,7 +60,7 @@ def get_hash_bucket_number(value, buckets):
 
 
 def add_value_to_hashmap(hashmap, value, buckets):
-    """Add value to the provided hash map with buckets number.
+    """Add value to the provided hash map with provided total buckets number.
     :param hashmap:
     :param value:
     :param buckets:
@@ -82,7 +83,7 @@ def add_value_to_hashmap(hashmap, value, buckets):
 
 # todo: implement save / load variations to / from file (cache)
 # todo: implement adding additional spec symbols
-def build_variations_list(buckets=0):
+def build_variations(buckets=0):
     """Build list of all possible variations of symbols for further search.
     :param buckets: number of buckets to divide symbols
     :return: list of variations
@@ -99,49 +100,10 @@ def build_variations_list(buckets=0):
 
     for letter1 in rus_chars + eng_chars + num_chars:
         for letter2 in rus_chars + eng_chars + num_chars:
-
-            #value = letter1 + letter2                     # generated value (without spec symbols)
             result = add_value_to_hashmap(result, letter1 + letter2, buckets)  # add value to hashmap bucket
 
-            # bucket_number = get_hash_bucket_number(value, buckets)  # bucket number for the value
-            # if result.get(bucket_number) is None:  # bucket is not initialized yet
-            #     result[bucket_number] = list()
-            # result.get(bucket_number).append(value)                 # add value to the bucket
-
             for spec_symbol in spec_symbols:
-                #value = letter1 + spec_symbol + letter2       # generated value (with spec symbols)
                 result = add_value_to_hashmap(result, letter1 + spec_symbol + letter2, buckets)  # add value to hashmap bucket
-
-                #single_list.append(letter1 + spec_symbol + letter2)
-
-
-    # # - process all characters
-    # if buckets <= 0:  # no buckets required for variations list
-    #     single_list = list()
-    #     result[0] = single_list  # resulting dictionary with just one list (all variations)
-    #
-    #     for letter1 in rus_chars + eng_chars + num_chars:
-    #         for letter2 in rus_chars + eng_chars + num_chars:
-    #             single_list.append(letter1 + letter2)
-    #             for spec_symbol in spec_symbols:
-    #                 single_list.append(letter1 + spec_symbol + letter2)
-    #
-    #     single_list.sort()  # sort resulting single list
-    #
-    # else:  # some buckets required - usually for multiple threads
-    #
-    #     result.get()
-    #     for letter1 in rus_chars + eng_chars + num_chars:
-    #         for letter2 in rus_chars + eng_chars + num_chars:
-    #             single_list.append(letter1 + letter2)
-    #             for spec_symbol in spec_symbols:
-    #                 single_list.append(letter1 + spec_symbol + letter2)
-    #
-    #         hash_object1 = hashlib.md5(b'Hello World').hexdigest()
-    #         hash_object2 = hashlib.md5('Hello World'.encode('utf-8')).hexdigest()
-    #         print("---> 1-1 ", hash_object1)
-    #         print("---> 1-2 ", int(hash_object1, 16))
-    #         print("---> 1-3 ", int(hash_object1, 16) % 5)
 
     return result
 
@@ -150,18 +112,26 @@ def perform_request(request_param):
     """Perform one HTTP POST request with one form parameter for search.
     :return: HTML output with found data
     """
+    log.debug('perform_request(): request param [{}].'.format(request_param))
+
+    if request_param is None or len(request_param.strip()) == 0:  # fail-fast - empty value
+        raise ValueError('Provided empty value [{}]!'.format(request_param))
+
     my_dict = {FORM_PARAM: request_param}             # dictionary for POST request
     data = parse.urlencode(my_dict).encode(ENCODING)  # perform encoding of request
     req = request.Request(MAIN_URL, data=data)        # this will make the method "POST" request
     context = ssl.SSLContext()                        # new SSLContext -> to bypass security certificate check
     response = request.urlopen(req, context=context)  # perform request itself
+
     return response.read().decode(ENCODING)           # read response and perform decode
 
 
 def parse_data(html):
-    """Parse HTML with search results.
-    :return: dictionary () with ships parsed from HTML response
+    """Parse HTML with one search request results and return dictionary with found ships, using
+    IMO number as a key.
+    :return: dictionary with ships parsed from HTML response
     """
+    log.debug('parse_data(): processing.')
 
     if not html:  # empty html response provided - return empty dictionary
         log.error("Returned empty HTML response!")
@@ -178,17 +148,14 @@ def parse_data(html):
 
     if table_body:
         table_rows = table_body.find_all('tr')  # find all rows <tr> inside a table body
-        #print("Found row(s): {}".format(len(table_rows)))
+        log.debug("Found row(s): {}".format(len(table_rows)))
 
         for row in table_rows:  # iterate over all found rows
-            #print("\nROW ===> ", row)
+            # log.debug("Processing row: [{}]".format(row))  # <- too much output
 
             if row:  # if row is not empty - process it
                 ship_dict = {}
                 cells = row.find_all('td')  # find all cells in the table row <tr>
-
-                # for col in cells:  # process all cells in the table row <tr>
-                #     print("COL ===> ", col, "val -> ", col.text)
 
                 # get ship parameters
                 ship_dict['flag'] = cells[0].img['title']        # get attribute 'title' of tag <img>
@@ -205,34 +172,43 @@ def parse_data(html):
     return ships_dict
 
 
-def perform_ships_search(symbols_variations):
-    """Process list of strings for the search.
+def perform_ships_search_single_thread(symbols_variations):
+    """Process list of strings for the search in single thread.
     :param symbols_variations: symbols variations for search
     :return: ships dictionary for the given list of symbols variations
     """
+    log.debug("perform_ships_search_single_thread(): perform single-threaded search.")
+
+    if symbols_variations is None or not isinstance(symbols_variations, dict):
+        raise ValueError('Provided empty dictionary [{}] or it isn\'t dictionary!'.format(symbols_variations))
+
     local_ships = {}
     counter = 1
-    variations_length = len(variations)
-    for search_string in symbols_variations:
+
+    variations_length = len(symbols_variations[0])
+
+    for search_string in symbols_variations[0]:
         log.debug("Currently processing: {} ({} out of {})".format(search_string, counter, variations_length))
         html = perform_request(search_string)  # request site and get HTML
         ships = parse_data(html)               # parse received data and get ships dictionary
         local_ships.update(ships)              # update main dictionary with found data
-        log.debug("Found ship(s): {}, total: {}, search string: {}"
-                  .format(len(ships), len(local_ships), search_string))
+
+        log.info("Found ship(s): {}, total: {}, search string: {}".format(len(ships), len(local_ships), search_string))
         counter += 1  # increment counter
 
     return local_ships
 
 
 def save_ships(xls_file, ships_map):
-    """Save search results into xls file
+    """Save provided search results into xls file.
     :param xls_file:
     :param ships_map:
     :return:
     """
-    if not ships_map:
-        log.warning("Provided empty ships map!")
+    log.debug('save_ships(): save provided ships map into file: {}.'.format(xls_file))
+
+    if ships_map is None:
+        log.warning("Provided empty ships map! Nothing to save!")
         return
 
     book = xlwt.Workbook()              # create workbook
@@ -266,51 +242,22 @@ def save_ships(xls_file, ships_map):
 
 
 # main part of the script
-if __name__ == '__main__':
-    print('Don\'t run this script directly!')
+# if __name__ == '__main__':
+#     print('Don\'t run this script directly! Use wrapper script!')
 
 main_ships = {}
+log.info('Starting [scrap_book] module...')
 
-hash_object1 = hashlib.md5(b'Hello World').hexdigest()
-hash_object2 = hashlib.md5('Hello World'.encode('utf-8')).hexdigest()
-print("---> 1-1 ", hash_object1)
-print("---> 1-2 ", int(hash_object1, 16))
-print("---> 1-3 ", int(hash_object1, 16) % 5)
+# build list of variations for search strings
+variations = build_variations()
+log.debug("Built list of variations: {}".format(variations))
+log.debug("# of built variations: {}".format(len(variations)))
 
-print("---> 2 ", hash_object2)
+# process search variations strings
+main_ships.update(perform_ships_search_single_thread(variations))
+log.debug("Processed all characters.")
+log.info("Found total ship(s): {}".format(len(main_ships)))
 
-print('Current script name: {}'.format(__name__))
-
-#build_variations_list(9)
-
-
-# log.info('Starting [scrap_book] module...')
-# log.debug('Ready to parse the site :)')
-#
-# # build list of variations for search strings
-# variations = build_variations_list()
-# log.debug("Built list of variations: {}".format(variations))
-# log.debug("# of built variations: {}".format(len(variations)))
-#
-# # process search variations strings
-# main_ships.update(perform_ships_search(variations))
-# log.debug("Processed all characters.")
-# log.info("Found total ship(s): {}".format(len(main_ships)))
-
-
-
-
-#
-# # process english characters
-# main_ships.update(process_chars(eng_chars))
-# log.debug("Processed english characters.")
-# log.info("Found ship(s): {}".format(len(main_ships)))
-#
-# # process numbers
-# main_ships.update(process_chars(num_chars))
-# log.debug("Processed numbers.")
-# log.info("Found ship(s): {}".format(len(main_ships)))
-#
-# # save to excel file
-# save_ships(OUTPUT_FILE, main_ships)
-# log.info("Saved ships to file {}".format(OUTPUT_FILE))
+# save to excel file
+save_ships(OUTPUT_FILE, main_ships)
+log.info("Saved ships to file {}".format(OUTPUT_FILE))
